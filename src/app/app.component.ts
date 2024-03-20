68fa72c1cf1ce2b8a437d76fdd4e6e5f8eff9a25
= <=>
2024-03-20 17:28:48 +0530

    geo code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { Geolocation } from '@capacitor/geolocation'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.requestPermission()
+  }
+
+  async requestPermission() {
+    const havePermission = await Geolocation.checkPermissions()
+    if (havePermission.location == 'granted') {
+      const { latitude, longitude } = await this.getCoordinates()
+
+    } else {
+      await Geolocation.requestPermissions()
+    }
+
+  }
+
+  async getCoordinates() {
+    const { coords: { latitude, longitude } } = await Geolocation.getCurrentPosition();
+    return { latitude, longitude }
+  }
+
+  async getLocation({ lat, long }) {
+    // await https://www.feroeg.com/address?lat=19.0748&lon=72.8856
+  }
 }
