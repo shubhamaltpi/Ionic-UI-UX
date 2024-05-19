9e867e69f67f5a2ea1529c7da9214c21efc221ee
Shubham Aher <139440042+shubhamaltpi@users.noreply.github.com>
2024-05-19 23:35:24 +0530

    Update zone-flags.ts

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -1,6 +1,2 @@
-/**
- * Prevents Angular change detection from
- * running with certain Web Component callbacks
- */
-// eslint-disable-next-line no-underscore-dangle
+
 (window as any).__Zone_disable_customElements = true;
